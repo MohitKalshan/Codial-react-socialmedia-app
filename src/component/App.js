0bac54de62d563a28d21f9f5f656bdb01b1b4949
MohitKalshan <mohitkalshan14.mk@gmail.com>
2023-05-25 22:55:30 +0530

    load data from api

--- src/component/App.js
+++ src/component/App.js
@@ -1,19 +1,27 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import {getPost, getPosts} from '../api';
 import { Home } from "../pages";
-
+import {Loader} from "./";
 function App() {
+ const [posts,setPosts]= useState([]);
+ const [loading,setLoading]= useState(true);
   useEffect(()=>{
-
     const fetchPost = async ()=>{
     const response = await getPosts()
     console.log('Response',response);
+    if(response.success){
+      setPosts(response.data.posts);
     }
+    setLoading(false);
+    };
     fetchPost();
   },[]);
+  if(loading){
+    return <Loader/>
+  }
   return (
     <div className="App">
-     <Home/>
+     <Home posts={posts}/>
     </div>
   );
 }
